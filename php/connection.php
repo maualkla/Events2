0c1f3abeac94b4caf6116a51735cf2718841f42f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-13 12:26:04 -0500

    inicio

---
+++ php/connection.php
@@ -0,0 +1,12 @@
+<?php
+/* Conection PHP */
+/* **********************************************************************************************
+	--> Events in PHP
+	--> Open Source proyect
+	--> Autor: @maualkla
+	--> Creation date: April 13th, 2019
+	--> Last Edition: April 13th, 2019
+	--> Description: Function to create a conection used in all the project.
+	--> Dependencies:   none 
+************************************************************************************************* */
+?>
