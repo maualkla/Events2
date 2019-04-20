4e4875ab2065797148bb6aaa1a95cd7da166bee9
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-20 16:09:29 -0500

    login

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+/* Index PHP */
+/* **********************************************************************************************
+	--> Events in PHP
+	--> Open Source proyect
+	--> Autor: @maualkla
+	--> Creation date: April 20th, 2019
+	--> Last Edition: April 20th, 2019
+	--> Description: Validate the user nickname and password, then create a session for the user or redirect it to the index.html page.
+	--> Dependencies:   connection.php ( For the DB connection )
+************************************************************************************************* */
+
+
+?>
