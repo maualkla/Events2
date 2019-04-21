6007c3a777fcb774c9e6ef085fc072bc4afc190f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-21 00:37:24 -0500

    ya_show_the_users_alv

--- users.php
+++ users.php
@@ -21,6 +21,20 @@
 		<div class="container">
 			<div class="" id="error-msg">
 				<?php
+					$param = ""; $param = $_REQUEST['param'];
+					$display = false;
+					if($param != "")
+					{
+						$sql = "SELECT * FROM user WHERE fname LIKE '%".$param."%' OR lname LIKE '%".$param."%' OR nickname LIKE '%".$param."%' OR userid LIKE '%".$param."%'";
+						require_once("php/connection.php");
+					    $result = mysqli_query($dbc,$sql) or die ("Error: " .mysqli_error($dbc));
+					    mysqli_close($dbc);
+					    $display = true;
+					}
+					else
+					{
+						// No seek
+					}
 				?>
 			</div>
 			<div class="top">
@@ -34,9 +48,26 @@
 				<div class="form">
 					<form action="users.php" method="POST">
 						<h5> USER ID OR NAME </h5>
-						<input type="text" placeholder="ID, NICKNAME OR NAME" value="" name="param"/>
+						<input type="text" placeholder="ID, NICKNAME OR NAME" value="" name="param">
 						<input type="submit" value="SEARCH"/>
 					</form>
+				</div>
+				<div class="content-table <?php if($display){echo'display';}else{echo 'hide';} ?>">
+					<table class="">
+						<tr>
+							<th> ID </th>
+							<th> Name </th>
+							<th> Lastname </th>
+							<th> Email </th>
+							<th> Nickname </th>
+						</tr>
+						<?php
+							while($row = mysqli_fetch_array($result, MYSQLI_BOTH)) 
+						    {
+						        echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td></tr>";
+						    }
+						?>
+					</table>
 				</div>
 			</div>
 		</div>
