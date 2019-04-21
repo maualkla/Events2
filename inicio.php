f70bdca899c18147230d76cc1f0beabce28251a0
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-20 21:23:38 -0500

    con_inicio

--- inicio.php
+++ inicio.php
@@ -1,3 +1,15 @@
+<?php
+/* Inicio PHP */
+/* **********************************************************************************************
+	--> Events in PHP
+	--> Open Source proyect
+	--> Autor: @maualkla
+	--> Creation date: April 20th, 2019
+	--> Last Edition: April 20th, 2019
+	--> Description: Main page of the Events system
+	--> Dependencies:   connection.php ( For the DB connection )
+************************************************************************************************* */
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,5 +17,6 @@
 	</head>
 	<body>
 		<h2>Here is the main page.</h2>
+		<h3><a href="php/close.php">Close Sesion</a></h3>
 	</body>
 </html>
