4a8142566d0fbdff1d88521946b350404a5ba540
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-22 18:03:29 -0500

    improving_alerts

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,70 +4,123 @@
 // V1 
 function alerts()
 {
+	console.log( " Hey im in alert ");
 	var url = new URL(window.location.href);
 	var param = url.searchParams.get("pe");
-	if(param == 1)
+	var text = "";
+	var war = "";
+	var show = false;
+
+	// New version
+	switch (param)
 	{
-		//alert(" Error, check your password ");
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Error, wrong user or password!";
-		text.classList.add("error");
+		case '1': 
+			text = 'Error, wrong user or password!'; war = 'er'; show = true;
+			break;
+		case '2': 
+			text = 'Sesion Closed!'; war = 'wr'; show = true;
+			break;
+		case '3': 
+			text = 'Login first!'; war = 'wr'; show = true;
+			break;
+		case '4': 
+			text = 'Isertion failed!'; war = 'er'; show = true;
+			break;
+		case '5': 
+			console.log( " Entro a 5");
+			text = 'User created!'; war = 'wr'; show = true;
+			break;
+		case '6': 
+			text = 'User updated!'; war = 'wr'; show = true;
+			break;
+		case '7': 
+			text = 'User deleted!'; war = 'wr'; show = true;
+			break;
+		case '8': 
+			text = 'Password Incorrect!'; war = 'er'; show = true;
+			break;
+		case '9': 
+			text = 'Passwords don´t match!'; war = 'er'; show = true;
+			break;
+		case '10': 
+			text = 'Password updated!'; war = 'wr'; show = true;
+			break;
+		default :
+		console.log("nada");
+			break;
 	}
-	else if(param == 2)
+
+	if(show)
 	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Sesion Closed";
-		text.classList.add("warning");
+		var msg = document.getElementById("error-msg")
+		msg.innerHTML = text;
+		msg.classList.add(war);
 	}
-	else if(param == 3)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Login first";
-		text.classList.add("warning");
-	}
-	else if(param == 4)
-	{
-		//alert(" Error, check your password ");
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Insertion failed";
-		text.classList.add("error");
-	}
-	else if(param == 5)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "User created";
-		text.classList.add("warning");
-	}
-	else if(param == 6)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "User updated";
-		text.classList.add("warning");
-	}
-	else if(param == 7)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "User deleted";
-		text.classList.add("warning");
-	}
-	else if(param == 8)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Password incorrect";
-		text.classList.add("error");
-	}
-	else if(param == 9)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Passwords don't match";
-		text.classList.add("error");
-	}
-	else if(param == 10)
-	{
-		var text = document.getElementById("error-msg")
-		text.innerHTML = "Password updated";
-		text.classList.add("warning");
-	}
+
+
+	// Old version
+	// if(param == 1)
+	// {
+	// 	//alert(" Error, check your password ");
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Error, wrong user or password!";
+	// 	text.classList.add("error");
+	// }
+	// else if(param == 2)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Sesion Closed";
+	// 	text.classList.add("warning");
+	// }
+	// else if(param == 3)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Login first";
+	// 	text.classList.add("warning");
+	// }
+	// else if(param == 4)
+	// {
+	// 	//alert(" Error, check your password ");
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Insertion failed";
+	// 	text.classList.add("error");
+	// }
+	// else if(param == 5)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "User created";
+	// 	text.classList.add("warning");
+	// }
+	// else if(param == 6)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "User updated";
+	// 	text.classList.add("warning");
+	// }
+	// else if(param == 7)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "User deleted";
+	// 	text.classList.add("warning");
+	// }
+	// else if(param == 8)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Password incorrect";
+	// 	text.classList.add("error");
+	// }
+	// else if(param == 9)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Passwords don't match";
+	// 	text.classList.add("error");
+	// }
+	// else if(param == 10)
+	// {
+	// 	var text = document.getElementById("error-msg")
+	// 	text.innerHTML = "Password updated";
+	// 	text.classList.add("warning");
+	// }
 }
 
 function confirmDelete(param)
