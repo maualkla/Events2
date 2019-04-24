a97c8a83eca3e8ee2c592ddb8bec75b360880f5f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2019-04-23 23:44:02 -0500

    solo_design

--- modules/event/play.php
+++ modules/event/play.php
@@ -1,20 +1,20 @@
 <?php include("../system/sesion.php"); ?>
 <?php
-/* Event PHP */
+/* Play PHP */
 /* **********************************************************************************************
 	--> Events in PHP
 	--> Open Source proyect
 	--> Autor: @maualkla
 	--> Creation date: April 23rd, 2019
 	--> Last Edition: April 23rd, 2019
-	--> Description: Main page of the Events module
+	--> Description: Page to view the events.
 	--> Dependencies:   connection.php ( For the DB connection ) 
 ************************************************************************************************* */
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Event</title>
+		<title>Play Event</title>
 		<link rel="shortcut icon" href="/Events/assets/events-beta-icon.png">
 		<link rel=StyleSheet href= "/Events/css/design.css" type="text/css">
 		<script type = "text/javascript" src="/Events/javascript/functions.js"></script>
@@ -26,12 +26,10 @@
 				?>
 			</div>
 			<div class="top">
-				<h2>Events main</h2>
+				<h2>Play Event</h2>
 			</div>
 			<div class="menu">
-				<h3><a href="../../inicio.php">Back to Home</a></h3>
-				<h3><a href="event.php?option=1">Create New Event</a></h3>
-				<h3><a href="play.php?param=0">Go To Event Screen</a></h3>
+				<h3><a href="event.php">Back to Events</a></h3>
 			</div>
 		</div>
 	</body>
